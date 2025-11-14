31a440a26e87f20fb573eeffd684406df6f678af
FiNTiKUglY <ilya.pronin.0202@gmail.com>
2025-11-15 00:40:06 +0300

    repair run.bat

--- ServerFiles/run.bat
+++ ServerFiles/run.bat
@@ -10,6 +10,18 @@ for /f "usebackq delims=" %%i in (".env") do (
     )
 )
 
+echo open ftp://%FTP_USER%:%FTP_PASS%@%FTP_HOST% > script.txt
+echo lcd "./" >> script.txt
+echo cd %REMOTE_DIR% >> script.txt
+echo option transfer binary >> script.txt
+echo get * >> script.txt
+echo exit >> script.txt
+
+"./WinSCP/winscp.com" /script=script.txt
+del script.txt
+
+java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.1/win_args.txt %*
+
 set "FILEMASK=|"
 for /f "usebackq delims=" %%i in ("%IGNORE_FILE%") do (
     set "LINE=%%i"
@@ -18,18 +30,6 @@ for /f "usebackq delims=" %%i in ("%IGNORE_FILE%") do (
     )
 )
 set "FILEMASK=!FILEMASK:~0,-1!"
-
-echo open ftp://%FTP_USER%:%FTP_PASS%@%FTP_HOST% > script.txt
-echo lcd "./" >> script.txt
-echo cd %REMOTE_DIR% >> script.txt
-echo option transfer binary >> script.txt
-echo get *
-echo exit >> script.txt
-
-"./WinSCP/winscp.com" /script=script.txt
-del script.txt
-
-java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.1/win_args.txt %*
 
 echo open ftp://%FTP_USER%:%FTP_PASS%@%FTP_HOST% > script.txt
 echo lcd "./" >> script.txt
